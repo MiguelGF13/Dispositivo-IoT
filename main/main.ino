dfa98c2f2e90fcdc8241aa3af8147689bbbae5cd
MiguelGF13 <magomezflores1@gmail.com>
2024-11-15 21:14:38 -0600

    se agrega el proyecto

---
+++ main/main.ino
@@ -0,0 +1,31 @@
+#include "thingProperties.h"
+
+void setup() {
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500); 
+
+  // Defined in thingProperties.h
+  initProperties();
+
+  // Connect to Arduino IoT Cloud
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  
+  /*
+     The following function allows you to obtain more information
+     related to the state of network and IoT Cloud connection and errors
+     the higher number the more granular information you’ll get.
+     The default is 0 (only errors).
+     Maximum is 4
+ */
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+}
+
+void loop() {
+  ArduinoCloud.update();
+  // Your code here
+  float analogValue = analogRead(36);
+  voltaje = analogValue*3.3/4095;
+}
